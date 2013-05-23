fce41e1c5025e35337fa4b8a1fe16720b309b808
Chris McGraw <chris.mcgraw@lodgenet.com>
2013-05-23 01:37:30 -0500

    gets password passing working, need to store hash with encryption

--- mitgr81com/unclear/models.py
+++ mitgr81com/unclear/models.py
@@ -2,14 +2,19 @@ import random
 import string
 
 from django.db import models
+from django.conf import settings
 from django.core.urlresolvers import reverse
+from Crypto.Cipher import AES
+from Crypto import Random
+
+from unclear import qaes
 
 
 class PassphraseHash(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     expire_at = models.DateTimeField(auto_now=True, editable=False)
-    passphrase = models.CharField(max_length=64)
-    unlock_phrase = models.CharField(max_length=64)
+    passphrase = models.CharField(max_length=255)
+    iv = models.CharField(max_length=16, editable=False)
     slug = models.SlugField(max_length=255, blank=True, default='', editable=False)
     max_access = models.IntegerField(default=1)
     access_count = models.IntegerField(default=0, editable=False)
@@ -24,9 +29,19 @@ class PassphraseHash(models.Model):
     def remaining_unlocks(self):
         return self.max_access - self.access_count
 
+    @property
+    def decry_passphrase(self):
+        return self.passphrase
+        return qaes.decrypt(self.passphrase, self.iv)
+
     def get_absolute_url(self):
         return reverse('unclear_thanks', args=[self.slug])
 
     def save(self, *args, **kwargs):
-        self.slug = u''.join([random.choice(string.ascii_letters + string.digits + '-_') for ch in range(8)])
+        if self.pk is None:  # Doing an insert
+            self.slug = u''.join([random.choice(string.ascii_letters + string.digits + '-_') for ch in range(8)])
+            self.iv = Random.new().read(AES.block_size)
+            # import pdb; pdb.set_trace()
+            # self.passphrase = unicode(qaes.encrypt(self.slug + settings.SECRET_KEY[:8], self.passphrase, self.iv))
+            print self.passphrase
         super(PassphraseHash, self).save(*args, **kwargs)
---
+++ mitgr81com/unclear/qaes.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# http://www.tylerlesmann.com/2008/dec/19/encrypting-database-data-django/
+import binascii
+import string
+from random import choice
+from Crypto.Cipher import AES
+
+EOD = '`%EofD%`' # This should be something that will not occur in strings
+
+def genstring(length=16, chars=string.printable):
+    return ''.join([choice(chars) for i in range(length)])
+
+def encrypt(key, s, iv):
+    obj = AES.new(key, AES.MODE_CFB, iv)
+    datalength = len(s) + len(EOD)
+    if datalength < 16:
+        saltlength = 16 - datalength
+    else:
+        saltlength = 16 - datalength % 16
+    ss = ''.join([s, EOD, genstring(saltlength)])
+    return binascii.b2a_base64(obj.encrypt(ss))
+
+def decrypt(key, s, iv):
+    obj = AES.new(key, AES.MODE_CFB, iv)
+    ss = obj.decrypt(binascii.a2b_base64(s))
+    return ss.split(EOD)[0]
+
+if __name__ == '__main__':
+    # for i in xrange(8, 20):
+        # s = genstring(i)
+        s = 'U2FsdGVkX19dKcS7eMVZyx0m1WL5gib1Blf7I3Le9s0Mw0Xztqouc3KpYaFrNoCm'
+        key = genstring(32)
+        print 'The key is', key
+        print 'The string is', s
+        cipher  = encrypt(key, s, 'here is an iv123')
+        print 'The encrypted string is', cipher
+        print 'This decrypted string is', decrypt(key, cipher, 'here is an iv123')
