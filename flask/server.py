49ada4e5cce9b5623bc5819252084f9193004cfb
Chris McGraw <chris.mcgraw@lodgenet.com>
2013-06-11 00:57:55 -0500

    Adds a flask implementation of unclear...at least partially

---
+++ flask/server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from flask import Flask
+from unclear.views import unclear_views
+
+app = Flask(__name__)
+app.register_blueprint(unclear_views, url_prefix='/unclear')
+
+@app.route('/')
+def index():
+    return "hello"
+
+@app.template_filter('plural')
+def plural(counter):
+    return 's' if counter > 1 else ''
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flask/unclear/views.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template
+from flask.views import MethodView
+
+unclear_views = Blueprint('unclear_views', __name__, template_folder='templates')
+
+
+class PassphraseView(MethodView):
+
+    def get(self, passphrase_id=None):
+        if passphrase_id is None:
+            return render_template('form.html')
+        else:
+            return render_template('unlock.html', object={})
+
+    def post(self):
+        # request.values['unlock_phrase']
+        # request.values['passphrase']
+        # request.values['max_access']
+        return render_template('thanks.html', full_uri="eeaosu", thing={'stuff': 'aabbcc112233'})
+
+    def patch(self, passphrase_id):
+        return "kk"
+
+
+unclear_views.add_url_rule('/', view_func=PassphraseView.as_view('unclear_views'))
+unclear_views.add_url_rule('/<passphrase_id>', view_func=PassphraseView.as_view('unclear_views'))
