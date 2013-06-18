7d3c83ef1f01154f4c4f3fb0d53fb3db91420419
Chris McGraw <chris.mcgraw@lodgenet.com>
2013-06-17 23:43:56 -0500

    Removes db the rest of the way

--- mitgr81com/server.py
+++ mitgr81com/server.py
@@ -1,11 +1,8 @@
 import importlib
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
-
-db = SQLAlchemy(app)
 
 
 def tryregister(app, module, prefix=''):
