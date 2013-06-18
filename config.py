c3244e34b87c036d85eec5e5aadb8db0a2c31cd7
Chris McGraw <chris.mcgraw@lodgenet.com>
2013-06-18 08:27:42 -0500

    Adds a make target for install

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-import os
-_basedir = os.path.abspath(os.path.dirname(__file__))
+# import os
+# _basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = False
 
@@ -8,5 +8,8 @@ SECRET_KEY = 'ty6dccqh^-9+yfunak!r6(g1ehbm7=wpds!9a!8kj((g=62@_y'
 
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
 SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/mitgr81com_dev'
+SQLALCHEMY_BINDS = {
+    'drupalchemy':        'mysqldb://root@localhost/cmcgraw_lodgenetcomwhatever',
+}
 
 DATABASE_CONNECT_OPTIONS = {}
