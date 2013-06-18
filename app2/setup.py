c8ea2c5e4a45445acf5a2792b75350b54bf5cc17
Chris McGraw <chris.mcgraw@lodgenet.com>
2013-06-17 23:16:08 -0500

    Unfocuses the input element when the phrase is unlocked
    It happened that I couldn't recover a test passphrase because I keyed-up in that
    field with a tab or something.  Let's stop that

--- app2/setup.py
+++ app2/setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
+import app2
 
 
 setup(
     name="app2",
+    version=app2.version,
     packages=find_packages(),
     namespace_packages=['app2'],
     )
--- unclear/setup.py
+++ unclear/setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
-
+import unclear
 
 setup(
     name="unclear",
+    version=unclear.version,
     packages=find_packages(),
     namespace_packages=['unclear'],
-    )
+)
