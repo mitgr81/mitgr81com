40e3ee4778d105eab74c9de66373400c4cb4d685
Chris McGraw <chris.mcgraw@lodgenet.com>
2013-06-25 11:20:45 -0500

    Fixes some stuffs

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'ty6dccqh^-9+yfunak!r6(g1ehbm7=wpds!9a!8kj((g=62@_y'
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
 SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/mitgr81com_dev'
 SQLALCHEMY_BINDS = {
-    'drupalchemy':        'mysqldb://root@localhost/cmcgraw_lodgenetcomwhatever',
+    'drupalchemy':        'mysql://root@localhost/cmcgraw_lodgenetcomwhatever',
 }
 
 DATABASE_CONNECT_OPTIONS = {}
